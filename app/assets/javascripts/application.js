cf51fcddc979daacb37dcccbefd9625a7eafbbca
Rajib Chowdhury <chowdhury284@gmail.com>
2013-06-17 01:53:34 +0530

    initial UI added.'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.nivo.slider.pack
 //= require_tree .
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @new_three_product = Product.find(:all, :limit => 3)
   end
 end
